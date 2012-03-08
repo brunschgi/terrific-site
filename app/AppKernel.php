dad80f997db0fecae66b130bdb8322ab215e22c5
Remo Brunschwiler <remo.brunschwiler@namics.com>
2012-03-08 18:21:33 +0100

    remove copy logic

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,59 +36,10 @@ class AppKernel extends Kernel
 
         if (in_array($this->getEnvironment(), array('dev'))) {
             // here comes your dev only dependencies
-
-            // update terrific resources
-            $dir = __DIR__.'/../src/Terrific/Module/';
-
-            $finder = new Finder();
-            $finder->directories()->in($dir)->depth('== 0');
-
-            // deploy composition resources
-            @mkdir(__DIR__ .'/../web/bundles');
-            // $this->recursiveDelete(__DIR__ .'/../web/bundles/terrificcomposition');
-            $this->recursiveCopy(__DIR__.'/../src/Terrific/Composition/Resources/public', __DIR__ .'/../web/bundles/terrificcomposition');
-
-            foreach ($finder as $file) {
-                // deploy module resources
-                // $this->recursiveDelete(__DIR__ .'/../web/bundles/terrificmodule'.strtolower($file->getFilename()));
-                $this->recursiveCopy($file->getRealpath().'/Resources/public', __DIR__ .'/../web/bundles/terrificmodule'.strtolower($file->getFilename()));
-            }
         }
 
         return $bundles;
     }
-
-    private function recursiveCopy($src,$dst) {
-        $dir = opendir($src);
-        @mkdir($dst);
-        while(false !== ( $file = readdir($dir)) ) {
-            if (( $file != '.' ) && ( $file != '..' ) && ( strpos($file, '.') !== 0)) {
-                if ( is_dir($src . '/' . $file) ) {
-                    $this->recursiveCopy($src . '/' . $file,$dst . '/' . $file);
-                }
-                else {
-                    copy($src . '/' . $file, $dst . '/' . $file);
-                }
-            }
-        }
-        closedir($dir);
-    }
-
-    private function recursiveDelete($target) {
-        $dir = opendir($target);
-        while(false !== ( $file = readdir($dir)) ) {
-            if (( $file != '.' ) && ( $file != '..' )) {
-                if ( is_dir($target . '/' . $file) ) {
-                    $this->recursiveDelete($target . '/' . $file);
-                }
-                else {
-                    unlink($target . '/' . $file);
-                }
-            }
-        }
-        rmdir($target);
-    }
-
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
