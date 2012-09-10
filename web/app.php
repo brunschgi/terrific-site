d3587fecc7a3dc77cd25305c869b9d6d5fb386d5
Remo Brunschwiler <remo@terrifically.org>
2012-09-10 17:13:52 +0200

    enable site caching

--- web/app.php
+++ web/app.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+require_once __DIR__.'/../app/AppCache.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
